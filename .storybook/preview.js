65b8c3dc4ffdaf11322635e2da987c513daa6782
Alexander Gusev <goooseman@me.com>
2020-06-09 17:27:26 +0300

    chore(storybook/addons): add addon-console

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import { addDecorator, addParameters } from "@storybook/react";
 import StorybookSharedWrapper from "../src/wrappers/StorybookSharedWrapper";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { withA11y } from "@storybook/addon-a11y";
+import "@storybook/addon-console";
 
 addDecorator(withA11y);
 
