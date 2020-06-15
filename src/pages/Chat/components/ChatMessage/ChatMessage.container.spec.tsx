a46555c450c30a24dbc9660e891b3ef249fb3c51
Alexander Gusev <goooseman@me.com>
2020-06-15 21:09:45 +0300

    feat(chat): show image if image link is pasted

--- src/pages/Chat/components/ChatMessage/ChatMessage.container.spec.tsx
+++ src/pages/Chat/components/ChatMessage/ChatMessage.container.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ChatMessageContainer from "./ChatMessage.container";
 import { render } from "__utils__/render";
+import nock from "nock";
 
 const defaultMessage = {
   text: "Yo!",
@@ -12,6 +13,17 @@ const defaultMessage = {
 
 it("should find links automatically", () => {
   const textWithLink = "Find it out on Google: https://google.com!";
+  const { getByText } = render(
+    <ChatMessageContainer {...defaultMessage} text={textWithLink} />
+  );
+  expect(getByText("https://google.com")).toHaveAttribute(
+    "href",
+    "https://google.com"
+  );
+});
+
+it("should find links in brackets automatically", () => {
+  const textWithLink = "Find it out on Google: (https://google.com!)";
   const { getByText } = render(
     <ChatMessageContainer {...defaultMessage} text={textWithLink} />
   );
@@ -43,14 +55,15 @@ it("should find links with path automatically", () => {
     "https://google.com/page?foo=bar&time=am"
   );
 });
+
 it("should find links with port number automatically", () => {
-  const textWithLink = "Find it out on Google: https://google.com:80!";
+  const textWithLink = "Find it out on Google: http://google.com:80!";
   const { getByText } = render(
     <ChatMessageContainer {...defaultMessage} text={textWithLink} />
   );
-  expect(getByText("https://google.com:80")).toHaveAttribute(
+  expect(getByText("http://google.com:80")).toHaveAttribute(
     "href",
-    "https://google.com:80"
+    "http://google.com:80"
   );
 });
 
@@ -63,4 +76,47 @@ it("should find links with http proto automatically", () => {
     "href",
     "http://google.com"
   );
+});
+
+describe("Images", () => {
+  afterEach(nock.restore);
+
+  it("should find first image in the text automatically", async () => {
+    nock("https://images.unsplash.com")
+      .head("/photo-1542692810-396766644d8c")
+      .reply(200, {}, { "Content-type": "image/jpeg" });
+
+    const textWithLink =
+      "Here is a picture of a beautiful car: https://images.unsplash.com/photo-1542692810-396766644d8c";
+    const { findByAltText } = render(
+      <ChatMessageContainer {...defaultMessage} text={textWithLink} />
+    );
+    expect(await findByAltText("Image from the message")).toBeInTheDocument();
+    expect(await findByAltText("Image from the message")).toHaveAttribute(
+      "src",
+      "https://images.unsplash.com/photo-1542692810-396766644d8c"
+    );
+  });
+
+  it("should find second image in the text automatically", async () => {
+    nock("https://google.com")
+      .head("/")
+      .reply(200, {}, { "Content-type": "text/html" });
+    nock("https://images.unsplash.com")
+      .head("/photo-1542692810-396766644d8c")
+      .reply(200, {}, { "Content-type": "image/jpeg" });
+
+    const textWithLink =
+      "I've searched through google (https://google.com). Here is a picture of a beautiful car: https://images.unsplash.com/photo-1542692810-396766644d8c";
+    const { findByAltText } = render(
+      <ChatMessageContainer {...defaultMessage} text={textWithLink} />
+    );
+    expect(
+      await findByAltText("Image from the message", {}, { timeout: 5 * 1000 })
+    ).toBeInTheDocument();
+    expect(await findByAltText("Image from the message")).toHaveAttribute(
+      "src",
+      "https://images.unsplash.com/photo-1542692810-396766644d8c"
+    );
+  });
 });
--- src/pages/Chat/components/ChatMessage/ChatMessage.container.tsx
+++ src/pages/Chat/components/ChatMessage/ChatMessage.container.tsx
@@ -1,30 +1,93 @@
 import React, { PureComponent } from "react";
 import ChatMessage, { ChatMessageProps } from "./ChatMessage";
+import AwaitLock from "await-lock";
 
 interface ChatMessageContainerProps extends ChatMessageProps {
   text: string;
 }
 
-const linkRegexp = /(https?:\/\/[\w-\.\/\:\?\=\&]+)/gi;
+interface ChatMessageContainerState {
+  imageSrc?: string;
+}
 
-class ChatMessageContainer extends PureComponent<ChatMessageContainerProps> {
+const linkRegexp = /(https?:\/\/[\w-\.\/\:\?\=\&]+)/gi;
+const imageContentTypeRegexp = /^image\//;
+
+class ChatMessageContainer extends PureComponent<
+  ChatMessageContainerProps,
+  ChatMessageContainerState
+> {
+  public state: ChatMessageContainerState = {};
+  public isImageLinkCache: { [link: string]: boolean } = {};
+  public isImageLinkLock: AwaitLock = new AwaitLock();
+  public isUnmounted = false;
+
+  public componentWillUnmount(): void {
+    this.isUnmounted = true;
+  }
+
   render(): React.ReactNode {
-    return <ChatMessage {...this.props} text={this.getText()} />;
+    return (
+      <ChatMessage
+        {...this.props}
+        text={this.getText()}
+        imageSrc={this.state.imageSrc}
+      />
+    );
   }
 
   private getText = (): React.ReactNode => {
     const { text } = this.props;
+    const links: string[] = [];
     // https://stackoverflow.com/a/33238464
     const parts: (string | JSX.Element)[] = text.split(linkRegexp);
     for (let i = 1; i < parts.length; i += 2) {
+      links.push(parts[i] as string);
       parts[i] = (
         <a key={"link" + i} href={parts[i] as string}>
           {parts[i]}
         </a>
       );
     }
+    void this.checkLinksForImages(links);
+
     return parts;
   };
+
+  private checkLinksForImages = async (links: string[]): Promise<void> => {
+    for (const link of links) {
+      if (this.isUnmounted) {
+        return;
+      }
+      const isImageLink = await this.isImageLink(link);
+      if (isImageLink) {
+        this.setState({
+          imageSrc: link,
+        });
+        return;
+      }
+    }
+  };
+
+  private async isImageLink(link: string): Promise<boolean> {
+    await this.isImageLinkLock.acquireAsync();
+    try {
+      if (this.isImageLinkCache[link]) {
+        return this.isImageLinkCache[link];
+      }
+
+      const headResponse: Response = await window.fetch(link, {
+        method: "HEAD",
+      });
+      const contentType = headResponse.headers.get("Content-Type");
+      const result = imageContentTypeRegexp.test(contentType || "");
+
+      this.isImageLinkCache[link] = result;
+      return result;
+    } finally {
+      this.isImageLinkLock.release();
+    }
+  }
 }
 
 export default ChatMessageContainer;
