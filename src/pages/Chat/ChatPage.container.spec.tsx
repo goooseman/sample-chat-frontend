bc93d775368a16734223f9870acb8eccd0921951
Alexander Gusev <goooseman@me.com>
2020-06-15 12:19:03 +0300

    feat(pages/chat): redirect user to settings if no username is set

---
+++ src/pages/Chat/ChatPage.container.spec.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ChatPageContainer from "./ChatPage.container";
+import { render } from "__utils__/renderWithRouter";
+import { withRouter } from "react-router-dom";
+
+const Container = withRouter(ChatPageContainer);
+
+it("should not redirect to /settings", () => {
+  const { history } = render(<Container username="goooseman" />);
+  expect(history.location.pathname).toBe("/");
+});
+
+it("should redirect to /settings if no username is provided", () => {
+  const { history } = render(<Container username={undefined} />);
+  expect(history.location.pathname).toBe("/settings");
+});
+
+it("should redirect to /settings if username is empty", () => {
+  const { history } = render(<Container username="" />);
+  expect(history.location.pathname).toBe("/settings");
+});
--- src/pages/Chat/ChatPage.container.tsx
+++ src/pages/Chat/ChatPage.container.tsx
@@ -1,14 +1,42 @@
 import React, { PureComponent } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, Redirect } from "react-router-dom";
 import ChatPage from "./ChatPage";
+import { WithSettings, withSettings } from "src/contexts/SettingsContext";
+import { Route } from "src/config/routes";
 
-interface ChatPageContainerProps extends RouteComponentProps {}
+interface ChatPageContainerProps extends RouteComponentProps, WithSettings {}
 
-class ChatPageContainer extends PureComponent<ChatPageContainerProps> {
-  render(): React.ReactNode {
+interface ChatPageContainerState {
+  redirectTo?: Route;
+}
+
+class ChatPageContainer extends PureComponent<
+  ChatPageContainerProps,
+  ChatPageContainerState
+> {
+  public state: ChatPageContainerState = {};
+
+  public componentDidMount() {
+    this.setRedirectIfUsernameIsEmpty();
+  }
+
+  public componentDidUpdate() {
+    this.setRedirectIfUsernameIsEmpty();
+  }
+
+  public render(): React.ReactNode {
+    if (this.state.redirectTo) {
+      return <Redirect to={this.state.redirectTo} />;
+    }
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     return <ChatPage chatMessages={[]} onSubmit={() => {}} />;
   }
+
+  private setRedirectIfUsernameIsEmpty() {
+    if (!this.props.username || this.props.username === "") {
+      this.setState({ redirectTo: "/settings" });
+    }
+  }
 }
 
-export default ChatPageContainer;
+export default withSettings(ChatPageContainer);
