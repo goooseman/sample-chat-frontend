6c568d04c2bfddc74263a7015498e7b5c20444dc
Alexander Gusman <alex@goooseman.ru>
2021-03-06 17:04:26 +0200

    improvement(search): create gstate library

---
+++ src/utils/gstate/gstate.spec.ts
@@ -0,0 +1,80 @@
+import { createMachine, GMachine } from "./gstate";
+
+let machine: GMachine<["off", "on"], ["switch"]>;
+
+let transitionActionSpy: jest.Mock;
+let onEnterSpy: jest.Mock;
+let onExitSpy: jest.Mock;
+
+beforeEach(() => {
+  transitionActionSpy = jest.fn();
+  onEnterSpy = jest.fn();
+  onExitSpy = jest.fn();
+  machine = createMachine({
+    off: {
+      transitions: {
+        switch: {
+          target: "on",
+          action: transitionActionSpy,
+        },
+      },
+      actions: {
+        onEnter: onEnterSpy,
+        onExit: onExitSpy,
+      },
+    },
+    on: {
+      transitions: {
+        switch: {
+          target: "off",
+          action: transitionActionSpy,
+        },
+      },
+      actions: {
+        onEnter: onEnterSpy,
+        onExit: onExitSpy,
+      },
+    },
+    initialState: "off",
+  });
+});
+
+it("should use initialState as default value", () => {
+  expect(machine.value).toBe("off");
+});
+
+it("should contain transiton function which returns value", () => {
+  expect(machine.transition("off", "switch")).toBe("on");
+});
+
+it("should switch the state", () => {
+  let state = machine.value;
+  state = machine.transition(state, "switch");
+  expect(state).toBe("on");
+  state = machine.transition(state, "switch");
+  expect(state).toBe("off");
+});
+
+it("should fire action", () => {
+  let state = machine.value;
+  state = machine.transition(state, "switch");
+  expect(transitionActionSpy).toBeCalledTimes(1);
+  machine.transition(state, "switch");
+  expect(transitionActionSpy).toBeCalledTimes(2);
+});
+
+it("should fire onEnter", () => {
+  let state = machine.value;
+  state = machine.transition(state, "switch");
+  expect(onEnterSpy).toBeCalledTimes(1);
+  machine.transition(state, "switch");
+  expect(onEnterSpy).toBeCalledTimes(2);
+});
+
+it("should fire onExit", () => {
+  let state = machine.value;
+  state = machine.transition(state, "switch");
+  expect(onExitSpy).toBeCalledTimes(1);
+  machine.transition(state, "switch");
+  expect(onExitSpy).toBeCalledTimes(2);
+});
---
+++ src/utils/gstate/gstate.ts
@@ -0,0 +1,43 @@
+export interface GMachine<T extends string[], E extends string[]> {
+  value: T[number];
+  transition: (currentState: T[number], event: E[number]) => T[number];
+}
+
+type MachineOptions<T extends string[], E extends string[]> = {
+  [stateName in T[number]]: {
+    actions?: {
+      onEnter?: () => void;
+      onExit?: () => void;
+    };
+    transitions: {
+      [eventName in E[number]]: {
+        target: T[number];
+        action?: () => void;
+      };
+    };
+  };
+} & {
+  initialState: T[number];
+};
+
+export const createMachine = <T extends string[], E extends string[]>(
+  definition: MachineOptions<T, E>
+): GMachine<T, E> => {
+  const machine = {
+    value: definition.initialState,
+    transition: (currentState: T[number], event: E[number]) => {
+      const currStateDefinition = definition[currentState];
+      const destTransition = currStateDefinition.transitions[event];
+      if (!destTransition) {
+        return machine.value;
+      }
+      const destState = destTransition.target;
+      machine.value = destState;
+      destTransition.action?.();
+      currStateDefinition.actions?.onExit?.();
+      definition[destState].actions?.onEnter?.();
+      return machine.value;
+    },
+  };
+  return machine;
+};
